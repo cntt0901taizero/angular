c250912a7056820e7d36da414757747e614786a2
chu the tai <thetaichu@hotmail.com>
2022-07-28 14:50:35 +0700

    1

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContentfulService } from 'src/app/service/contentful.service';
+import { SharedModule } from 'src/shared/shared.module';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome.component';
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule, CommonModule],
+  imports: [WelcomeRoutingModule, CommonModule, SharedModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent],
   providers: [ContentfulService]
